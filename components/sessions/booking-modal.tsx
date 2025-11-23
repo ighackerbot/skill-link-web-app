f56c8660ac2556f260c01468c7d052d6f836e2cb
ighackerbot <jainanuj2005@gmail.com>
2025-11-23 22:47:52 +0530

    problem resolution

--- components/sessions/booking-modal.tsx
+++ components/sessions/booking-modal.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { FormEvent, useEffect, useMemo, useState } from "react"
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "../ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
